54b95aa2f6ff9651af10b8444854478ee10557dd
oomaar <omaar_5@hotmail.com>
2025-08-10 16:29:57 +0300

    feat(FormInput.tsx): Add reusable FormInput component with label, error display, and controlled input handling

---
+++ src/components/FormInput/FormInput.tsx
@@ -0,0 +1,35 @@
+import React, { ChangeEvent, HTMLInputTypeAttribute } from "react";
+import {
+  FormInputContainer,
+  FormInputErrorText,
+  FormInputInput,
+  FormInputLabel,
+  FormInputWapper,
+} from "./styled-form-input";
+
+type FormInputProps = {
+  label: string;
+  type: HTMLInputTypeAttribute;
+  value: string | number;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  errorState?: {
+    isError: boolean;
+    errorMessage: string;
+  };
+};
+
+export function FormInput(props: FormInputProps) {
+  const { label, type, value, onChange, errorState } = props;
+
+  return (
+    <FormInputContainer>
+      <FormInputLabel>{label}</FormInputLabel>
+      <FormInputWapper $isError={errorState?.isError}>
+        <FormInputInput type={type} value={value} onChange={onChange} />
+      </FormInputWapper>
+      {errorState && (
+        <FormInputErrorText>{errorState.errorMessage}</FormInputErrorText>
+      )}
+    </FormInputContainer>
+  );
+}
