73612bc4090e0924f7bd57b09c3e5eeb2466ebff
oomaar <omaar_5@hotmail.com>
2025-08-10 16:48:48 +0300

    update(ScreenHeader.tsx): Add optional buttonElement prop to ScreenHeader component for rendering custom buttons alongside the title.

--- src/components/ScreenHeader/ScreenHeader.tsx
+++ src/components/ScreenHeader/ScreenHeader.tsx
@@ -1,15 +1,18 @@
 import { ScreenHeaderContainer } from "./styled-screen-header";
+import { ReactNode } from "react";
 
 type ScreenHeaderProps = {
   title: string;
+  buttonElement?: ReactNode;
 };
 
 export const ScreenHeader = (props: ScreenHeaderProps) => {
-  const { title } = props;
+  const { title, buttonElement } = props;
 
   return (
     <ScreenHeaderContainer>
       <p>{title}</p>
+      {buttonElement && buttonElement}
     </ScreenHeaderContainer>
   );
 };
