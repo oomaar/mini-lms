cc7d9ee9ee069fff7947bbcfbff8f3547ffc2b8f
oomaar <omaar_5@hotmail.com>
2025-08-10 13:03:48 +0300

    feat(Table.tsx): create reusable Table component with header, body, and pagination integration

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,57 @@
+import React, { ReactElement } from "react";
+import {
+  TableContainer,
+  TableTable,
+  TableWrapper,
+  TBody,
+} from "./styled-table";
+import { TableHeader } from "./TableHeader/TableHeader";
+import { TablePagination } from "./TablePagination/TablePagination";
+
+type TableProps = {
+  header: {
+    title: string;
+    searchTerm: string;
+    onSearch: (searchTerm: string) => void;
+    placeholder?: string;
+  };
+  pagination: {
+    totalRowsCount: number;
+    tableRowsPerPage: number;
+    setTableRowsPerPage: (tableRowsPerPage: number) => void;
+    currentPage: number;
+    setCurrentPage: (currentPage: number) => void;
+  };
+  tableHeadings: ReactElement;
+  tableRows: ReactElement;
+};
+
+export function Table(props: TableProps) {
+  const { header, tableHeadings, tableRows, pagination } = props;
+
+  return (
+    <TableContainer>
+      <TableHeader
+        searchTerm={header.searchTerm}
+        onSearch={header.onSearch}
+        title={header.title}
+        placeholder={header.placeholder}
+      />
+      <TableWrapper>
+        <TableTable>
+          <thead>
+            <tr>{tableHeadings}</tr>
+          </thead>
+          <TBody>{tableRows}</TBody>
+        </TableTable>
+      </TableWrapper>
+      <TablePagination
+        totalRowsCount={pagination.totalRowsCount}
+        tableRowsPerPage={pagination.tableRowsPerPage}
+        setTableRowsPerPage={pagination.setTableRowsPerPage}
+        currentPage={pagination.currentPage}
+        setCurrentPage={pagination.setCurrentPage}
+      />
+    </TableContainer>
+  );
+}
