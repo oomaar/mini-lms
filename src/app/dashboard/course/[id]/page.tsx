9e16b53db571de410a13dc67a7a5d03a1dda59ae
oomaar <omaar_5@hotmail.com>
2025-08-10 17:27:59 +0300

    feat(single course page.tsx): render CreateLessonPopup if user is Admin

--- src/app/dashboard/course/[id]/page.tsx
+++ src/app/dashboard/course/[id]/page.tsx
@@ -5,18 +5,27 @@ import { useCourseById } from "@/hooks/useCourses/useCourseById";
 import { useParams } from "next/navigation";
 import { SingleCourseLessonsTable } from "./_components/SingleCourseLessonsTable";
 import styled from "styled-components";
+import { CreateLessonPopup } from "./_components/CreateLessonPopup/CreateLessonPopup";
+import { useLocalStorageUser } from "@/hooks/useLocalStorageUser";
 
 export default function SingleCoursePage() {
   const { id } = useParams() as { id: string };
 
   const { data: course, isLoading, error } = useCourseById(id);
 
+  const { user } = useLocalStorageUser();
+  const userRole = user?.userRole;
+  const isUserAdmin = userRole === "Admin";
+
   if (isLoading) return <Loading />;
   if (error) return <p>{(error as Error).message}</p>;
 
   return (
     <div>
-      <ScreenHeader title={course?.title || ""} />
+      <ScreenHeader
+        title={course?.title || ""}
+        buttonElement={isUserAdmin && <CreateLessonPopup courseId={id} />}
+      />
       <DescriptionText>{course?.description}</DescriptionText>
       <SingleCourseLessonsTable
         lessonIds={course?.lessonIds || []}
